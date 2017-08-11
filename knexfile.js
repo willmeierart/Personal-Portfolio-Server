55531f1222cc37bbecb9a81be6b82b2ba7426e18
WILL MEIER <willmeierart@gmail.com>
2017-08-11 16:04:59 -0600

    more init

--- knexfile.js
+++ knexfile.js
@@ -1,44 +1,11 @@
-// Update with your config settings.
-
+const dotenv = require('dotenv')
 module.exports = {
-
   development: {
-    client: 'sqlite3',
-    connection: {
-      filename: './dev.sqlite3'
-    }
+    client: 'pg',
+    connection: 'postgres://localhost/portfolio'
   },
-
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  },
-
   production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
+    client: 'pg',
+    connection: process.env.DATABASE_URL + '?ssl=true'
   }
-
-};
+}
